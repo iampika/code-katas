54caf3c65a4754acdaa86bc6a6f744f319b63c1f
Mahendra Choudhary <jakepintu@gmail.com>
2019-12-07 09:36:08 +0530

    Add Solution

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Kata are code challenges focused on improving skill and technique.
 
+- [Sum of positive](./sum-of-positive.js)
 - [Even or Odd](./even-or-odd.js)
 - [Sum Mixed Array](./sum-mixed-array.js)
---
+++ sum-of-positive.js
@@ -0,0 +1,12 @@
+// Sum of positive
+// https://www.codewars.com/kata/sum-of-positive/train/javascript
+
+function positiveSum(arr) {
+  return arr.reduce((p, c) => (c > 0 ? p + c : p), 0);
+}
+
+console.log(positiveSum([1, 2, 3, 4, 5]) === 15);
+console.log(positiveSum([1, -2, 3, 4, 5]) === 13);
+console.log(positiveSum([]) === 0);
+console.log(positiveSum([-1, -2, -3, -4, -5]) === 0);
+console.log(positiveSum([-1, 2, 3, 4, -5]) === 9);
